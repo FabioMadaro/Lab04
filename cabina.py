c2583106bb772c7bc19fdd6c1f6f2d3a7a36b495
FabioMadaro <fabiomadaro.05@gmail.com>
2025-10-15 14:20:51 +0200

    modificheLab

---
+++ cabina.py
@@ -0,0 +1,48 @@
+class Cabina:
+    def __init__(self, codice, letti, ponte, prezzo_base, extra = None, disponibile = True):
+        self.codice = codice
+        self.letti = int(letti)
+        self.ponte = int(ponte)
+        self.prezzo_base = float(prezzo_base)
+        self.extra = extra
+        self.disponibile = disponibile
+
+    def tipo(self):
+        if self.extra is None:
+            return "Standard"
+        elif str(self.extra).isdigit():
+            return "Animali"
+        else:
+            return f"Deluxe ({self.extra})"
+
+    def sovrapprezzo(self):
+        if self.extra is None:
+            prezzo = self.prezzo_base
+        elif str(self.extra).isdigit():
+            max_animali = int(self.extra)
+            prezzo = self.prezzo_base * (1 + 0.10 * max_animali)
+        else:
+            prezzo = self.prezzo_base * 1.20
+
+        return prezzo
+
+
+    def __str__(self):
+        if self.disponibile:
+            stato = "Disponibile"
+        else:
+            stato = "Occupata"
+
+        tipo_cabina = self.tipo()
+        prezzo = self.sovrapprezzo()
+
+        base = f"{self.codice}: {tipo_cabina} | {self.letti} letti - ponte {self.ponte} - prezzo {prezzo}€"
+
+        if self.extra is None:
+            descrizione = f"{base} - {stato}"
+        elif self.extra.isdigit():
+            descrizione = f"{base} - Max Animali: {self.extra} - {stato}"
+        else:
+            descrizione = f"{base} - {stato}"
+
+        return descrizione
--- crociera.py
+++ crociera.py
@@ -1,7 +1,12 @@
+from cabina import Cabina
+from passeggero import Passeggero
+
 class Crociera:
     def __init__(self, nome):
         """Inizializza gli attributi e le strutture dati"""
         # TODO
+        self.cabine = []
+        self.passeggeri = []
 
     """Aggiungere setter e getter se necessari"""
     # TODO
@@ -9,6 +14,25 @@ class Crociera:
     def carica_file_dati(self, file_path):
         """Carica i dati (cabine e passeggeri) dal file"""
         # TODO
+        try:
+            file = open(file_path, "r", encoding="utf-8")
+        except FileNotFoundError:
+            return None
+
+        for line in file:
+            campi = line.split(",")
+            if len(campi) == 3:
+                codice = campi[0]
+                nome = campi[1]
+                cognome = campi[2]
+                passeggero = Passeggero(codice, nome, cognome)
+                self.passeggeri.append(passeggero)
+            else:
+                codice = campi[0]
+                letti = campi[1]
+                ponte = campi[2]
+
+
 
     def assegna_passeggero_a_cabina(self, codice_cabina, codice_passeggero):
         """Associa una cabina a un passeggero"""
